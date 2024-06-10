cceb41c2de2362e2074583a10a35eb5f850f0c99
Hamdallah <hamdallahjodah@gmail.com>
2024-06-11 02:20:03 +0300

    chore: Update EasyLaravelPdf configuration URL

--- config/easy-laravel-pdf.php
+++ config/easy-laravel-pdf.php
@@ -2,5 +2,5 @@
 
 // config for Jouda/EasyLaravelPdf
 return [
-    "url" => env('EASY_LARAVEL_HTML_TO_PDF_URL', 'http://localhost:3000'),
+    "url" => env('EASY_LARAVEL_HTML_TO_PDF_URL', 'http://localhost:3000/html-to-pdf'),
 ];
