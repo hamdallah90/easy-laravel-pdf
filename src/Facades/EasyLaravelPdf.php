2a92cfb3e78c73184d9398fcb38195a87ca9471b
Hamdallah <hamdallahjodah@gmail.com>
2024-06-11 02:08:34 +0300

    Update EasyLaravelPdf facade with additional methods and options

--- src/Facades/EasyLaravelPdf.php
+++ src/Facades/EasyLaravelPdf.php
@@ -6,6 +6,15 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \Jouda\EasyLaravelPdf\EasyLaravelPdf
+ * 
+ * @method static \Jouda\EasyLaravelPdf\EasyLaravelPdf setOptions(array $options)
+ * @method static \Jouda\EasyLaravelPdf\EasyLaravelPdf addOption(string $key, mixed $value)
+ * @method static \Jouda\EasyLaravelPdf\EasyLaravelPdf setPuppeteerLunchArgs(array $puppeteerLunchArgs)
+ * @method static \Jouda\EasyLaravelPdf\EasyLaravelPdf loadView(string $view, array $data = [])
+ * @method static \Jouda\EasyLaravelPdf\EasyLaravelPdf loadHtml(string $html)
+ * @method static \Illuminate\Http\UploadedFile getFile()
+ * @method static \Jouda\EasyLaravelPdf\EasyLaravelPdf save(string $path)
+ * @method static \Jouda\EasyLaravelPdf\EasyLaravelPdf stream(string $filename)
  */
 class EasyLaravelPdf extends Facade
 {
